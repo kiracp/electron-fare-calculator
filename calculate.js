3bc4481d4a26deb889dc6cac20072af72546b772
Kira Prentice <kprentice@ath018369.corp.athenahealth.com>
2016-12-02 13:35:22 -0500

    form design

---
+++ calculate.js
@@ -0,0 +1,29 @@
+function calculate(x) {
+  var amt;
+  if (x >= 5.5) {
+      amt = x / 1.011
+  } else { amt = x }
+  // Hoping that mta rounds up
+  return Math.round(amt * 100)/100
+}
+
+function amount(x, numRides, newCard) {
+  var amt;
+  if (numRides) {
+    x = x * 2.75;
+  } if (newCard) {
+    amt = calculate(x) + 1;
+  } else {
+    amt = calculate(x);
+  }
+  return amt.toFixed(2);
+}
+
+function check(x) {
+  var amt;
+  if (x >= 5.5) {
+      amt = x * 1.011
+  } else { amt = x }
+  // Hoping that mta rounds up
+  return Math.round(amt * 100)/100
+}
